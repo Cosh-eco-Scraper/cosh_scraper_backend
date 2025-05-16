d880f54eb39b3b17d9393953bd694c840595dbef
MatteoBoulanger-ucll <matteo.boulanger@student.ucll.be>
2025-05-16 13:58:13 +0200

    prisma setup+ description model made

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,19 @@
 import express from 'express';
 import defaultRoutes from './routes/defaultRoutes';
 import llmRoutes from './routes/llm.routes'; // Assuming you have a llm.routes.ts file
-import {errorHandler} from './middlewares/errorHandler';
+import { errorHandler } from './middlewares/errorHandler';
 
 
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './swagger.json';
 
 const app = express();
-
 app.use(express.json());
 
 // Routes
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/default', defaultRoutes);
-app.use('/api/llm', llmRoutes); 
+app.use('/api/llm', llmRoutes);
 
 // Global error handler (should be after routes)
 app.use(errorHandler);
