5d2d3eda0c72956d79dc9c2eef79730f9cec82e9
MatteoBoulanger-ucll <matteo.boulanger@student.ucll.be>
2025-05-19 23:06:36 +0200

    a beginning

--- src/repositories/brand.repository.ts
+++ src/repositories/brand.repository.ts
@@ -4,35 +4,6 @@ import { brandMapper } from "./mapper";
 import { brandQueries } from "./queries/brands.queries";
 
 export const BrandRepository = {
-    getAllBrands: async () => {
-        let brands: Brand[] = [];
-
-        try {
-            databaseClient.connect();
-            const result = await databaseClient.query(brandQueries.getAllBrands());
-            brands = result.rows.map(brandMapper.mapBrands);
-            return brands;
-        } finally {
-            databaseClient.end();
-        }
-    },
-
-    getBrandById: async (id: number) => {
-        try {
-            databaseClient.connect();
-            const result = await databaseClient.query(brandQueries.getBrandById(id));
-
-            if (!result.rows.length) {
-                throw new Error("Brand not found");
-            }
-
-            const brand = brandMapper.mapBrands(result.rows[0]);
-            return brand;
-        } finally {
-            databaseClient.end();
-        }
-    },
-
     updateBrand: async (brandId: number, name: string, label: string) => {
         try {
             databaseClient.connect();
--- src/repositories/queries/brands.queries.ts
+++ src/repositories/queries/brands.queries.ts
@@ -1,13 +1,4 @@
 export const brandQueries = {
-    getBrandById: (id: number) => `SELECT *
-                                    FROM brands
-                                    WHERE id = ${id};`,
-    getAllBrands: () => `SELECT *
-                         FROM brands;`,
-    getBrandByName: (name: string) => `SELECT *
-                                       FROM brands
-                                       WHERE name = '${name}';`,
-
     updateBrand: (brandId: number, name: string, label: string) => `UPDATE brands 
                                                     SET name = '${name}',
                                                         label = '${label}'
