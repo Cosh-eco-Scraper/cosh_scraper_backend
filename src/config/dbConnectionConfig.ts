165fd0fb36fc4c8af0a6291b9a83bc529f5dd67b
MatteoBoulanger-ucll <113980608+MatteoBoulanger-ucll@users.noreply.github.com>
2025-05-19 12:18:34 +0200

    generated Ai description wordt gestored in the store table, not null … (#43)
    generated Ai description wordt gestored in the store table, not null values zijn nog hardcoded

---
+++ src/config/dbConnectionConfig.ts
@@ -0,0 +1,12 @@
+const { Client } = require('pg');
+
+
+const databaseClient = new Client({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
+
+export default databaseClient;
--- src/services/llm.service.ts
+++ src/services/llm.service.ts
@@ -1,5 +1,5 @@
 import { GoogleGenAI } from "@google/genai";
-
+import databaseClient from "../config/dbConnectionConfig";
 const ai = new GoogleGenAI({ apiKey: "AIzaSyA2dYJVOQNBmh9Ne2b3lnuO-Fbw7oSAV6U" });
 
 const sendPrompt = async (prompt: string): Promise<string | undefined> => {
@@ -8,8 +8,18 @@ const sendPrompt = async (prompt: string): Promise<string | undefined> => {
     contents: prompt,
   });
   console.log(response.text);
-    return response.text;
+
+  databaseClient.connect();
+
+  const query = "INSERT INTO stores (name,description, location_id) VALUES ($1, $2, $3)";
+  const values = ["test Store", response.text, "test location"];
+  const result = await databaseClient.query(query, values);
+  console.log(result);
+  await databaseClient.end();
+  return response.text;
 }
+
+
 
 
 export const LLMService = {
