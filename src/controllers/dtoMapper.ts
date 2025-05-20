73464e2b1e802d122ae380a09a56fa09f7b97fe1
MatteoBoulanger-ucll <matteo.boulanger@student.ucll.be>
2025-05-20 11:39:09 +0200

    Update dtoMapper.ts

--- src/controllers/dtoMapper.ts
+++ src/controllers/dtoMapper.ts
@@ -1,5 +1,6 @@
-import { Brand } from '../domain/Brand';
 import { Store, StoreDto } from '../domain/Store';
+import { DatabaseOpeningHours, OpeningHours, OpeningHoursDto } from '../domain/OpeningHours';
+import { BrandDto, DatabaseBrand } from '../domain/Brand';
 
 export const dtoMapper = {
   mapStore: (store: Store): StoreDto => {
@@ -10,12 +11,21 @@ export const dtoMapper = {
     };
   },
 
-  mapBrand: (brand: Brand): Brand => {
+  mapOpeningHours: (openingHours: DatabaseOpeningHours): OpeningHoursDto => {
+    return {
+      id: openingHours.id,
+      day: openingHours.day.name,
+      openingAt: openingHours.openingAt,
+      closingAt: openingHours.closingAt,
+      storeId: openingHours.storeId
+    };
+  },
+  mapBrand: (brand: DatabaseBrand): BrandDto => {
     return {
       id: brand.id,
       name: brand.name,
       label: brand.label,
+      storeId: brand.storeId
     };
-
   }
 };
