11f47b58290e11ef419f6402cb5e843f365613c8
Liam Omen <liam.omen@student.ucll.be>
2025-05-22 13:18:02 +0200

    update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,27 @@
 import js from '@eslint/js';
 import prettier from 'eslint-plugin-prettier';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default [
   js.configs.recommended,
   {
     files: ['**/*.js', '**/*.ts'],
     plugins: {
-      prettier: prettier
+      prettier,
+      'unused-imports': unusedImports,
     },
     rules: {
       'no-unused-vars': 'error',
-      'no-unused-imports': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
       'newline-before-return': 'error',
       'prettier/prettier': [
         'error',
@@ -18,13 +29,13 @@ export default [
           bracketSpacing: true,
           semi: true,
           singleQuote: true,
-          trailingComma: 'all'
-        }
-      ]
+          trailingComma: 'all',
+        },
+      ],
     },
     languageOptions: {
       ecmaVersion: 2024,
-      sourceType: 'module'
-    }
-  }
+      sourceType: 'module',
+    },
+  },
 ];
