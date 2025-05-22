a565e9617eb8cc7295c22ae0fe805ba1c434aa44
Liam Omen <liam.omen@student.ucll.be>
2025-05-22 13:44:01 +0200

    eslintrc.js added

---
+++ eslintrc.js
@@ -0,0 +1,31 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
+    curly: ['error', 'all'],
+    'no-console': 'off',
+  },
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  overrides: [
+    {
+      files: ['*.ts'],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: __dirname,
+      },
+      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    },
+  ],
+};
