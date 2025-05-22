c31db6af3b6020b0baf36976c8777c08ab2f2b6a
MatteoBoulanger-ucll <matteo.boulanger@student.ucll.be>
2025-05-22 09:50:31 +0200

    updating openingshours work

--- src/controllers/openinghours.controller.ts
+++ src/controllers/openinghours.controller.ts
@@ -4,17 +4,17 @@ import OpeningHoursService from "../services/openingshours.service";
 export async function updateOpeningHours(req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
     const { openingHoursId } = req.params;
-    const { day, startTime, endTime, store_id } = req.body;
+    const { day, startTime, endTime, storeId } = req.body;
 
     if (!openingHoursId) {
       res.status(400).json({ message: 'Opening hours ID is required' });
     }
 
-    if (!day || !startTime || !endTime || !store_id) {
+    if (!day || !startTime || !endTime || !storeId) {
       res.status(400).json({ message: "Day, startTime, endTime, and store_id are required" });
     }
 
-    await OpeningHoursService.updateOpeningHours(Number(openingHoursId), day, startTime, endTime, store_id);
+    await OpeningHoursService.updateOpeningHours(Number(openingHoursId), day, startTime, endTime, storeId);
     res.status(200).json({ message: 'Opening hours updated successfully' });
   } catch (error) {
     next(error);
