48f75661eba8c8121a44ce146310b741c1f07522
MatteoBoulanger-ucll <matteo.boulanger@student.ucll.be>
2025-05-26 11:45:56 +0200

    preventing sql injection

--- src/repositories/queries/location.querries.ts
+++ src/repositories/queries/location.querries.ts
@@ -1,27 +1,17 @@
-export const locationQuerries = {
-  updateLocation: (
-    locationId?: number,
-    street?: string,
-    number?: string,
-    postalCode?: string,
-    city?: string,
-    country?: string,
-  ) =>
-    `UPDATE locations
-         SET street = '${street}',
-             number = '${number}',
-             postal_code = '${postalCode}',
-             city = '${city}',
-             country = '${country}'
-         WHERE id = ${locationId};`,
-  createLocation: (
-    street: string,
-    number: string,
-    postalCode: string,
-    city: string,
-    country: string,
-  ) =>
-    `INSERT INTO locations (street, number, postal_code, city, country)
-              VALUES ('${street}', '${number}', '${postalCode}', '${city}', '${country}')
-              RETURNING *;`,
+export const locationQueries = {
+  updateLocation: () => `
+    UPDATE locations
+    SET street = $1,
+        number = $2,
+        postal_code = $3,
+        city = $4,
+        country = $5
+    WHERE id = $6;
+  `,
+
+  createLocation: () => `
+    INSERT INTO locations (street, number, postal_code, city, country)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *;
+  `,
 };
--- src/repositories/queries/store.queries.ts
+++ src/repositories/queries/store.queries.ts
@@ -1,59 +1,70 @@
 export const storeQueries = {
-  getStoreById: (id: number) => `SELECT s.id,
-                              s.name,
-                              s.description,
-                              l.street,
-                              l.id         as location_id,
-                              l.street,
-                              l.number,
-                              l.city,
-                              l.postal_code,
-                              l.country,
-                              l.created_at as loc_created_at,
-                              l.updated_at as loc_updated_at
-                       FROM stores s
-                              JOIN locations l ON s.location_id = l.id
-                                 WHERE s.id = ${id};`,
-  getAllStores: () => `SELECT s.id,
-                              s.name,
-                              s.description,
-                              l.street,
-                              l.id         as location_id,
-                              l.street,
-                              l.number,
-                              l.city,
-                              l.postal_code,
-                              l.country,
-                              l.created_at as loc_created_at,
-                              l.updated_at as loc_updated_at
-                       FROM stores s
-                              JOIN locations l ON s.location_id = l.id;`,
-  getStoreWithOpeningsHoursById(id: number) {
-    return `SELECT s.id          as store_id,
-                                                                     oh.id         as id,
-                                                                     oh.day        as "day",
-                                                                     oh.openingat  as opening_at,
-                                                                     oh.closingat  as closing_at,
-                                                                     oh.created_at as created_at,
-                                                                     oh.updated_at as updated_at
-                                                              FROM stores s
-                                                                     JOIN opening_hours oh ON oh.store_id = s.id
-                                                              WHERE s.id = ${id};`;
-  },
-  getBrandsByStoreId(id: number) {
-    return `select b.id, b.name, b.label, sb.store_id
-            from stores s
-                   join store_brands sb on sb.store_id = s.id
-                   join brands b on b.id = sb.brand_id
-            WHERE s.id = ${id};`;
-  },
-  updateStore: (storeId?: number, name?: string, description?: string) =>
-    `UPDATE stores
-                                                       SET name = '${name}',
-                                                       description = '${description}'
-                                                       WHERE id = ${storeId};`,
-  createStore: (name: string, location_id: number, description?: string) =>
-    `INSERT INTO stores (name, location_id, description)
-                                                           VALUES ('${name}', '${location_id}', '${description}')
-                                                           RETURNING *;`,
+       getStoreById: () => `
+         SELECT s.id,
+                s.name,
+                s.description,
+                l.street,
+                l.id         as location_id,
+                l.street,
+                l.number,
+                l.city,
+                l.postal_code,
+                l.country,
+                l.created_at as loc_created_at,
+                l.updated_at as loc_updated_at
+         FROM stores s
+                JOIN locations l ON s.location_id = l.id
+         WHERE s.id = $1;
+       `,
+
+       getAllStores: () => `
+         SELECT s.id,
+                s.name,
+                s.description,
+                l.street,
+                l.id         as location_id,
+                l.street,
+                l.number,
+                l.city,
+                l.postal_code,
+                l.country,
+                l.created_at as loc_created_at,
+                l.updated_at as loc_updated_at
+         FROM stores s
+                JOIN locations l ON s.location_id = l.id;
+       `,
+
+       getStoreWithOpeningsHoursById: () => `
+         SELECT s.id          as store_id,
+                oh.id         as id,
+                oh.day        as "day",
+                oh.openingat  as opening_at,
+                oh.closingat  as closing_at,
+                oh.created_at as created_at,
+                oh.updated_at as updated_at
+         FROM stores s
+                JOIN opening_hours oh ON oh.store_id = s.id
+         WHERE s.id = $1;
+       `,
+
+       getBrandsByStoreId: () => `
+         SELECT b.id, b.name, b.label, sb.store_id
+         FROM stores s
+                JOIN store_brands sb ON sb.store_id = s.id
+                JOIN brands b ON b.id = sb.brand_id
+         WHERE s.id = $1;
+       `,
+
+       updateStore: () => `
+         UPDATE stores
+         SET name = $1,
+             description = $2
+         WHERE id = $3;
+       `,
+
+       createStore: () => `
+         INSERT INTO stores (name, location_id, description)
+         VALUES ($1, $2, $3)
+         RETURNING *;
+       `,
 };
