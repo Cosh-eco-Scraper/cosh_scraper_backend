2228eec5f50800a75d0c2880dab6ba1826c8bf16
Liam Omen <liam.omen@student.ucll.be>
2025-05-28 16:49:14 +0200

    adding afternoon hours

--- src/scraper/scraper.ts
+++ src/scraper/scraper.ts
@@ -7,19 +7,53 @@ import getKeywords from './keywords';
 
 dotenv.config();
 const ai = new GoogleGenAI({ apiKey: process.env.AI_API_KEY });
-
 type ScrapedInfo = {
   url: string;
   name: string;
   brands: string[];
   openingHours: {
-    monday: { open: string; close: string } | null;
-    tuesday: { open: string; close: string } | null;
-    wednesday: { open: string; close: string } | null;
-    thursday: { open: string; close: string } | null;
-    friday: { open: string; close: string } | null;
-    saturday: { open: string; close: string } | null;
-    sunday: { open: string; close: string } | null;
+    monday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
+    tuesday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
+    wednesday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
+    thursday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
+    friday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
+    saturday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
+    sunday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
   };
   location: string;
   about: string;
