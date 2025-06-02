6dd8736582510d642b643b55b75d02a5e9cfd082
Liam-Omen <liam.omen@student.ucll.be>
2025-06-02 19:49:58 +0200

    change model

--- src/scraper/scraper.ts
+++ src/scraper/scraper.ts
@@ -13,13 +13,48 @@ type ScrapedInfo = {
   name: string;
   brands: string[];
   openingHours: {
-    monday: { open: string; close: string } | null;
-    tuesday: { open: string; close: string } | null;
-    wednesday: { open: string; close: string } | null;
-    thursday: { open: string; close: string } | null;
-    friday: { open: string; close: string } | null;
-    saturday: { open: string; close: string } | null;
-    sunday: { open: string; close: string } | null;
+    monday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
+    tuesday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
+    wednesday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
+    thursday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
+    friday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
+    saturday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
+    sunday: {
+      open: string;
+      close: string;
+      openAfterNoon: string | null;
+      closeAfterNoon: string | null;
+    } | null;
   };
   location: string;
   about: string;
