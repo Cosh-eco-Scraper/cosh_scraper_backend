b77ab890e39ec9ca1b5ccc2a2c4c556978e9939f
r0887562 <aaron.abbey@student.ucll.be>
2025-06-10 12:30:01 +0200

    removed(work for different branch

--- src/repositories/queries/storeBrands.querries.ts
+++ src/repositories/queries/storeBrands.querries.ts
@@ -5,10 +5,4 @@ export const storeBrandsQueries = {
       VALUES ($1, $2)
     `;
   },
-  removeBrandFromStore() {
-    return `
-      DELETE FROM store_brands
-      WHERE store_id = $1 AND brand_id = $2
-    `;
-  },
 };
