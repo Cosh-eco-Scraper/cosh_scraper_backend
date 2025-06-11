821eaab346e9ae265d8e30293bda816fe4c6a549
r0887562 <aaron.abbey@student.ucll.be>
2025-06-11 12:53:34 +0200

    brand name endpoint

--- src/routes/brand.routes.ts
+++ src/routes/brand.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getAllBrands, updateBrand } from '../controllers/brand.controller';
+import { getAllBrands, getBrandByName, updateBrand } from '../controllers/brand.controller';
 
 const router = Router();
 
 router.get('/', getAllBrands);
 router.put('/:brandId', updateBrand);
+router.get('/:name', getBrandByName);
 
 export default router;
