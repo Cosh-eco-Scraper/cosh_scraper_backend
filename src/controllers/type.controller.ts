f841f44e4a2d88b80049515756bd3844dfb4d91a
Liam-Omen <liam.omen@student.ucll.be>
2025-06-13 12:12:13 +0200

    add 204

--- src/controllers/type.controller.ts
+++ src/controllers/type.controller.ts
@@ -5,6 +5,12 @@ import { dtoMapper } from './dtoMapper';
 export async function GetAllTypes(_req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
     const types = await TypeService.getAllTypes();
+
+    if (!types.length) {
+      res.status(204);
+      return;
+    }
+
     res.json(types.map(dtoMapper.mapType));
     return;
   } catch (error) {
