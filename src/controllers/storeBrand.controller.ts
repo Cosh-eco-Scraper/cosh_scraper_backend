d7cbda2f9f6e86d3d607b31e2b6399222f5605db
Liam-Omen <liam.omen@student.ucll.be>
2025-06-13 18:49:20 +0200

    backend changes

--- src/controllers/storeBrand.controller.ts
+++ src/controllers/storeBrand.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import storeBrandsService from '../services/storeBrands.service';
 
 export async function addBrandsToStore(req: Request, res: Response, next: NextFunction) {
@@ -20,25 +20,27 @@ export async function addBrandsToStore(req: Request, res: Response, next: NextFu
 }
 export async function removeBrandFromStore(req: Request, res: Response, next: NextFunction) {
   try {
-    const storeId = req.params.storeId;
-    const brandId = req.params.brandId;
+    const { storeId, brandId } = req.body;
+    if (!storeId && !brandId) {
+      res.status(400).json({ message: 'Store ID and Brand ID are required' }).end();
+      return;
+    }
 
     if (!storeId) {
-      res.status(400).json({ message: 'Store ID is required' });
+      res.status(400).json({ message: 'Store ID is required' }).end();
       return;
     }
 
     if (!brandId) {
-      res.status(400).json({ message: 'Brand ID is required' });
+      res.status(400).json({ message: 'Brand ID is required' }).end();
       return;
     }
-
     const updatedStoreId = await storeBrandsService.removeBrandFromStore(
       parseInt(storeId),
       parseInt(brandId),
     );
 
-    res.status(200).json({ rowAffected: updatedStoreId });
+    res.status(200).json({ rowAffected: updatedStoreId }).end();
     return;
   } catch (error) {
     next(error);
